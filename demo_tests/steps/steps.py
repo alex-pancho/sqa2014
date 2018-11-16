ef93be9a5dbfba8fd818ed7172076fb6e5ec5e8d
panix <panix.p@yandex.ru>
2018-11-16 18:56:55 +0200

    Up to 2018 date
    add xpath location for elements

--- demo_tests/steps/steps.py
+++ demo_tests/steps/steps.py
@@ -9,7 +9,7 @@ def open_url(context, url):
     
 @when('нажать на ссылку "{link_text}"')
 def link_click(context, link_text):
-    link = context.browser.find_element_by_link_text(link_text)
+    link = context.browser.find_element_by_xpath('//div[@class="lang-switch"]/a')
     assert link, 'ссылка ' + link_text + ' не найдена'
     link.click()
     
@@ -20,6 +20,6 @@ def assert_url(context, url):
     
 @then('на странице будет текст "{text}"')
 def find_text(context, text):
-    page = context.browser.find_element_by_css_selector('body')
+    page = context.browser.find_element_by_xpath('//div[@class="slogan"]')
     page_text = page.text
-    assert text in page_text, 'на странице не найден текст: ' + text
+    assert text in page_text[:len(text)], 'на странице не найден текст: ' + text
